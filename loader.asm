4e4fc5f80f1487d280296e3cc2e925bf6d3e8641
CatMe0w <CatMe0w@live.cn>
2024-12-25 21:49:58 -0700

    Switch to a pure 32-bit solution

--- loader.asm
+++ loader.asm
@@ -40,27 +40,16 @@ _loader_start:
     mov gs, ax
     mov ss, ax
 
-    ; 4. Set up a 4-level paging structure
+    ; 4. Set up paging structure
     call setup_paging
 
-    ; 5. Enable PAE
-    mov eax, cr4
-    or eax, 0x20        ; CR4.PAE = 1
-    mov cr4, eax
-
-    ; 6. Enable long mode
-    mov ecx, 0xC0000080 ; IA32_EFER MSR
-    rdmsr
-    or eax, 0x100       ; Set LME (Long Mode Enable) bit
-    wrmsr
-
-    ; 7. Enable paging
+    ; 5. Enable paging
     mov eax, cr0
     or eax, 0x80000000  ; CR0.PG = 1
     mov cr0, eax
 
-    ; 8. Into 64-bit code segment
-    jmp 0x08:long_mode_entry
+    ; 6. Jump to the kernel
+    jmp 0x08:0x00200000
 
 hang:
     ; Infinite loop if an error occurs
@@ -70,231 +59,64 @@ hang_loop:
     jmp hang_loop
 
 ; -------------------------------
-; Set up 4-level paging structure
+; Set up paging structure
 ; -------------------------------
 setup_paging:
-    pushad              ; Save registers (32-bit)
+    pushad               ; Save registers
 
-    ; 1. CLEAR all entries in each 512-entry table
-
-    ; Clear PML4 (512 entries * 8 bytes = 4096 bytes)
-    xor eax, eax
-    mov edi, pml4_table
-    mov ecx, 512
-.zero_pml4:
-    mov dword [edi], eax     ; low 4 bytes
-    mov dword [edi + 4], eax ; high 4 bytes
-    add edi, 8
-    loop .zero_pml4
-
-    ; Clear PDP
-    mov edi, pdp_table
-    mov ecx, 512
-.zero_pdp:
-    mov dword [edi], eax
-    mov dword [edi + 4], eax
-    add edi, 8
-    loop .zero_pdp
+    ; 1. CLEAR all entries in each 1024-entry table
 
     ; Clear PD
+    xor eax, eax
     mov edi, pd_table
-    mov ecx, 512
+    mov ecx, 1024
 .zero_pd:
     mov dword [edi], eax
-    mov dword [edi + 4], eax
-    add edi, 8
+    add edi, 4
     loop .zero_pd
 
     ; Clear PT
     mov edi, pt_table
-    mov ecx, 512 * 32
+    mov ecx, 1024
 .zero_pt:
     mov dword [edi], eax
-    mov dword [edi + 4], eax
-    add edi, 8
+    add edi, 4
     loop .zero_pt
 
-    ; 2. MAP KERNEL: 0xFFFFFF8000200000 -> kernel_phys_base
-    ;    We'll use these indices:
-    ;      PML4[511] -> PDP
-    ;      PDP[0]    -> PD
-    ;      PD[1]     -> PT
-    ;      PT[0]     -> kernel_phys_base
-    ;    To get this:
-    ;      va = 0xFFFFFF8000200000
-    ;      pml4_index = (va >> 39) & 0x1FF = 511
-    ;      pdp_index  = (va >> 30) & 0x1FF = 0
-    ;      pd_index   = (va >> 21) & 0x1FF = 1
-    ;      pt_index   = (va >> 12) & 0x1FF = 0
-    ;      offset     = va & 0xFFF = 0
+    ; 2. MAP PD[0] -> PT[0] (0x00000000 -> 0x00000000)
+    mov eax, pt_table    ; No need to shift, as pt_table is 4KB-aligned
+    or eax, 0x3          ; Present + RW
+    mov dword [pd_table], eax
 
-    ; PML4[511] = &pdp_table | 0x03 (Present + RW)
-    mov eax, pdp_table  ; low 32 bits of address
-    mov edx, 0          ; high 32 bits (assuming <4GB)
-    or  eax, 0x3        ; set Present + RW
-    mov dword [pml4_table + (511 * 8)], eax
-    mov dword [pml4_table + (511 * 8) + 4], edx
-
-    ; PDP[0] = &pd_table | 0x03
-    mov eax, pd_table
-    mov edx, 0
-    or  eax, 0x3
-    mov dword [pdp_table + (0 * 8)], eax
-    mov dword [pdp_table + (0 * 8) + 4], edx
-
-    ; Map 64 MB for the kernel
-    ; 32 PD entries (32 * 2MB = 64MB), with 512 PT entries each (512 * 4KB = 2MB)
-
-    ; Load the kernel physical address from .bss
+    ; 3. MAP KERNEL: 0x00200000 -> kernel_phys_base
+    ;    We will identity-map the first 2MB of memory
     mov ebx, [kernel_phys_base]
-
-    lea edi, [pd_table + 8]     ; Skip 1 PD entry
-    mov esi, pt_table
-    mov ecx, 32
-.fill_kernel_pd:
-    mov eax, esi
-    or eax, 0x3
-    mov [edi], eax
-    mov dword [edi + 4], 0
-    add esi, 0x1000             ; Next PT
-    add edi, 8                  ; Next PD entry
-    loop .fill_kernel_pd
-
-    ; For each PD entry:
-    ; PT[0] = kernel_phys_base | 0x03
-    ; PT[...] = kernel_phys_base + ...
-    ; PT[511] = kernel_phys_base + 511 * 4KB | 0x03
-    xor edi, edi                ; i = 0
-    mov edx, 32                 ; 32 PD entries
-.fill_kernel_pt:
-    push edx
-
-    mov eax, pt_table
-    add eax, edi
-    mov esi, eax                ; PT[i]
-
+    mov edi, pt_table
+    add edi, (0x200 * 4) ; PT[0x200]
+    mov ecx, 512         ; 512 * 4KB = 2MB
+.fill_kernel:
     mov eax, ebx
-    add eax, edi                ; kernel_phys_base + (i * 2MB)
-
-    mov ecx, 512
-.fill_kernel_pt_inner:
-    mov ebp, eax
-    or ebp, 0x3
-    mov [esi], ebp
-    mov dword [esi + 4], 0
-    add eax, 0x1000             ; Next page
-    add esi, 8                  ; Next PT entry
-    loop .fill_kernel_pt_inner
-
-    add edi, 0x1000             ; Next 2MB
-    pop edx
-    dec edx
-    jnz .fill_kernel_pt
-
-    ; 3. IDENTITY-MAP the Loader @ 0x00100000
-    ;    We'll do PML4[0], PDP[0], PD[0], PT[256]
-
-    ; Zero out pdp_table_low, pd_table_low, pt_table_low
-    mov edi, pdp_table_low
-    mov ecx, 512
-.zero_pdp_low:
+    shr eax, 12          ; Truncate lower 12 bits
+    shl eax, 12
+    or eax, 0x3          ; Present + RW
     mov dword [edi], eax
-    mov dword [edi+4], eax
-    add edi, 8
-    loop .zero_pdp_low
+    add ebx, 0x1000      ; Next page
+    add edi, 4           ; Next PT entry
+    loop .fill_kernel
 
-    mov edi, pd_table_low
-    mov ecx, 512
-.zero_pd_low:
+    ; 4. IDENTITY-MAP the Loader @ 0x00100000
+    mov eax, 0x00100000  ; No need to shift
+    or eax, 0x3          ; Present + RW
+    mov edi, pt_table
+    add edi, (0x100 * 4) ; PT[0x100]
     mov dword [edi], eax
-    mov dword [edi+4], eax
-    add edi, 8
-    loop .zero_pd_low
 
-    mov edi, pt_table_low
-    mov ecx, 512
-.zero_pt_low:
-    mov dword [edi], eax
-    mov dword [edi+4], eax
-    add edi, 8
-    loop .zero_pt_low
-
-    ; PML4[0] = &pdp_table_low | 0x03
-    mov eax, pdp_table_low
-    mov edx, 0
-    or  eax, 0x3
-    mov dword [pml4_table + (0 * 8)], eax
-    mov dword [pml4_table + (0 * 8) + 4], edx
-
-    ; PDP[0] = &pd_table_low | 0x03
-    mov eax, pd_table_low
-    mov edx, 0
-    or  eax, 0x3
-    mov dword [pdp_table_low + (0 * 8)], eax
-    mov dword [pdp_table_low + (0 * 8) + 4], edx
-
-    ; PD[0] = &pt_table_low | 0x03
-    mov eax, pt_table_low
-    mov edx, 0
-    or  eax, 0x3
-    mov dword [pd_table_low + (0 * 8)], eax
-    mov dword [pd_table_low + (0 * 8) + 4], edx
-
-    ; PT[256] = 0x00100000 | 0x3
-    ; (256 * 4K = 0x100000)
-    mov eax, 0x00100000
-    mov edx, 0
-    or  eax, 0x3
-    mov dword [pt_table_low + (256 * 8)], eax
-    mov dword [pt_table_low + (256 * 8) + 4], edx
-
-    ; 4. LOAD pml4_table into CR3
-    mov eax, pml4_table
+    ; 5. LOAD pd_table into CR3
+    mov eax, pd_table
     mov cr3, eax
 
     popad
     ret
-
-[bits 64]
-long_mode_entry:
-    ; Now we are in 64-bit long mode (in a 64-bit code segment)
-
-    mov edi, 0x2BADB002 ; set multiboot magic, rust_entry() expects this
-    mov esi, 0          ; clear multiboot info, as current ArceOS doesn't use it
-
-    ; Mock bsp_entry32
-    ; Copied from /arceos/modules/axhal/src/platform/x86_pc/multiboot.S
-    lgdt [gdt_mock_descriptor]
-    ; set data segment selectors
-    mov     ax, 0x18
-    mov     ss, ax
-    mov     ds, ax
-    mov     es, ax
-    mov     fs, ax
-    mov     gs, ax
-
-    ; set PAE, PGE bit in CR4
-    ; mov     eax, {cr4}
-    ; mov     cr4, eax
-
-    ; load the temporary page table
-    ; lea     eax, [.Ltmp_pml4 - {offset}]
-    ; mov     cr3, eax
-
-    ; set LME, NXE bit in IA32_EFER
-    ; mov     ecx, {efer_msr}
-    ; mov     edx, 0
-    ; mov     eax, {efer}
-    ; wrmsr
-
-    ; set protected mode, write protect, paging bit in CR0
-    ; mov     eax, {cr0}
-    ; mov     cr0, eax
-
-    ; Hello, ArceOS!
-    mov rax, 0xffffff80002000b7 ; bsp_entry64
-    jmp rax
-    ; FIXME: ArceOS dies at init_memory_management(), the new page table set up by rust part is broken
 
 ; -------------------------------
 ; Data and paging structures
@@ -310,56 +132,19 @@ align 4
     ; Checksum (magic + flags + checksum = 0)
     dd -(0x1BADB002 + 0x00000001)
 
-section .data
+section .rodata
 align 16
 
 ; GDT: Null descriptor, Code descriptor, Data descriptor
 gdt_data:
     dq 0x0000000000000000 ; NULL descriptor
-    dq 0x00AF9A000000FFFF ; Code descriptor (64-bit)
-    dq 0x00CF92000000FFFF ; Data descriptor (32-bit)
     dq 0x00CF9A000000FFFF ; Code descriptor (32-bit)
+    dq 0x00CF92000000FFFF ; Data descriptor (32-bit)
 gdt_data_end:
 
 gdt_descriptor:
     dw (gdt_data_end - gdt_data - 1)
     dd gdt_data
-
-; bsp_entry32 GDT and paging structures
-; Copied from /arceos/modules/axhal/src/platform/x86_pc/multiboot.S
-gdt_mock_data:
-    dq 0x0000000000000000
-    dq 0x00cf9b000000ffff
-    dq 0x00af9b000000ffff
-    dq 0x00cf93000000ffff
-gdt_mock_data_end:
-
-gdt_mock_descriptor:
-    dw (gdt_mock_data_end - gdt_mock_data - 1)
-    dd gdt_mock_data
-
-; align 4096
-; Ltmp_pml4:
-;     # 0x0000_0000 ~ 0xffff_ffff
-;     .quad .Ltmp_pdpt_low - {offset} + 0x3   # PRESENT | WRITABLE | paddr(tmp_pdpt)
-;     .zero 8 * 255
-;     # 0xffff_8000_0000_0000 ~ 0xffff_8000_ffff_ffff
-;     .quad .Ltmp_pdpt_high - {offset} + 0x3  # PRESENT | WRITABLE | paddr(tmp_pdpt)
-;     .zero 8 * 255
-
-; Ltmp_pdpt_low:
-;     .quad 0x0000 | 0x83         # PRESENT | WRITABLE | HUGE_PAGE | paddr(0x0)
-;     .quad 0x40000000 | 0x83     # PRESENT | WRITABLE | HUGE_PAGE | paddr(0x4000_0000)
-;     .quad 0x80000000 | 0x83     # PRESENT | WRITABLE | HUGE_PAGE | paddr(0x8000_0000)
-;     .quad 0xc0000000 | 0x83     # PRESENT | WRITABLE | HUGE_PAGE | paddr(0xc000_0000)
-;     .zero 8 * 508
-
-; Ltmp_pdpt_high:
-;     .quad 0x0000 | 0x83         # PRESENT | WRITABLE | HUGE_PAGE | paddr(0x0)
-;     .quad 0x40000000 | 0x83     # PRESENT | WRITABLE | HUGE_PAGE | paddr(0x4000_0000)
-;     .quad 0x80000000 | 0x83     # PRESENT | WRITABLE | HUGE_PAGE | paddr(0x8000_0000)
-;     .quad 0xc0000000 | 0x83     # PRESENT | WRITABLE | HUGE_PAGE | paddr(0xc000_0000)
-;     .zero 8 * 508
 
 ; IDT: Empty (placeholder, will do nothing, let it reset if interrupts occur)
 idt_data:
@@ -373,18 +158,8 @@ idt_descriptor:
 section .bss
 align 4096
 
-; Top-level PML4 table
-pml4_table: resq 512
-
-; For high addresses (kernel)
-pdp_table: resq 512
-pd_table: resq 512
-pt_table: resq 32 * 512 * 8 ; Map many pages (64 MB) for the kernel
-
-; For low addresses (loader)
-pdp_table_low: resq 512
-pd_table_low: resq 512
-pt_table_low: resq 512
+pd_table: resd 1024
+pt_table: resd 1024
 
 ; Kernel physical base (set by loader)
 kernel_phys_base: resd 1
